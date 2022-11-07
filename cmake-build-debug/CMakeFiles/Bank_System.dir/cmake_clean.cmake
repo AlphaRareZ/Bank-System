b39141ae1a37bd2fe83c896dc7ccef0d3f1211fd
Mohamed Alaa Eddin Mostafa <dodo2003pro@gmail.com>
2022-11-07 18:35:41 +0200

    added empty files

---
+++ cmake-build-debug/CMakeFiles/Bank_System.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Bank_System.exe"
+  "Bank_System.exe.manifest"
+  "Bank_System.pdb"
+  "CMakeFiles/Bank_System.dir/main.cpp.obj"
+  "libBank_System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bank_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
